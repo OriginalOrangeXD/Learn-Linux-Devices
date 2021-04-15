bfeb17702394dadfa2eb1ae1857f0b317c6da046
Robert DeAngelis <deangelis.robert13@gmail.com>
2021-04-15 00:06:54 -0400

    errors

--- not-a-rootkit/scriptKiddieRootkit.c
+++ not-a-rootkit/scriptKiddieRootkit.c
@@ -5,6 +5,8 @@
 #include <linux/version.h>
 #include <linux/namei.h>
 
+#include "ftrace_helper.h"
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("TheXcelleratorXORANGE");
 MODULE_DESCRIPTION("mkdir syscall hook");
@@ -17,9 +19,9 @@ MODULE_VERSION("0.1");
 #ifdef PTREGS_SYSCALL_STUBS
 static asmlinkage long(*orig_mkdir)(const struct pt_regs *);
 
-asmlinkage int hook_mkdir(const struct pt_regs *reg)
+asmlinkage int hook_mkdir(const struct pt_regs *regs)
 {
-	char __user *pathname = (char *)reg->di;
+	char __user *pathname = (char *)regs->di;
 	char dir_name[NAME_MAX] = {0};
 	long error = strncpy_from_user(dir_name, pathname, NAME_MAX);
 	if(error > 0)
@@ -39,3 +41,24 @@ asmlinkage int hook_mkdir(const char __user *pathname,umode_t mode)
 	return 0;
 }
 #endif
+
+static struct ftrace_hook hooks[] = {
+	HOOK("sys_mkdir", hook_mkdir, &orig_mkdir),
+};
+
+static int __init rootkit_init(void)
+{
+	int err;
+	err = fh_install_hooks(hooks, ARRAY_SIZE(hooks));
+	if(err)
+		return err;
+	printk(KERN_INFO "rootkit: loaded\n");
+	return 0;
+}
+static void __exit rootkit_exit(void)
+{
+	fh_remove_hooks(hooks,ARRAY_SIZE(hooks));
+	printk(KERN_INFO "rootkit: unloaded\n");
+}
+module_init(rootkit_init);
+module_exit(rootkit_exit);
